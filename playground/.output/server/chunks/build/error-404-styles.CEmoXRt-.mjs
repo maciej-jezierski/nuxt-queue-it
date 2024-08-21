afae05a02aece2f15327fd25c817a12a117072e7
Maciej Jezierski <maciej.jezier@gmail.com>
2024-08-21 12:07:31 +0200

    feat: remove build files

--- playground/.output/server/chunks/build/error-404-styles.CEmoXRt-.mjs
+++
@@ -1,6 +0,0 @@
-import { e as error404_vue_vue_type_style_index_0_scoped_c6c1efc1_lang } from './error-404-styles-1.mjs-C3WU_nS9.mjs';
-
-const error404Styles_CEmoXRt_ = [error404_vue_vue_type_style_index_0_scoped_c6c1efc1_lang, error404_vue_vue_type_style_index_0_scoped_c6c1efc1_lang];
-
-export { error404Styles_CEmoXRt_ as default };
-//# sourceMappingURL=error-404-styles.CEmoXRt-.mjs.map
--- playground/.output/server/chunks/build/error-500-styles.Dq0oRtnV.mjs
+++
@@ -1,6 +0,0 @@
-import { e as error500_vue_vue_type_style_index_0_scoped_2c66185b_lang } from './error-500-styles-1.mjs-CO3eyqOT.mjs';
-
-const error500Styles_Dq0oRtnV = [error500_vue_vue_type_style_index_0_scoped_2c66185b_lang, error500_vue_vue_type_style_index_0_scoped_2c66185b_lang];
-
-export { error500Styles_Dq0oRtnV as default };
-//# sourceMappingURL=error-500-styles.Dq0oRtnV.mjs.map
